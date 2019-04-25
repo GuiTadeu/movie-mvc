8cf1672b4e5be05d3ae3fc1d1a6b07d104005852
Guilherme Tadeu <guitadeu.py@gmail.com>
2019-04-25 15:26:43 -0300

    [ADD] Sessao, Lugar e Tipo de Ingresso

--- src/main/java/br/com/caelum/ingresso/model/Ingresso.java
+++ src/main/java/br/com/caelum/ingresso/model/Ingresso.java
@@ -3,16 +3,34 @@ package br.com.caelum.ingresso.model;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-import br.com.caelum.ingresso.model.desconto.Desconto;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
+@Entity
 public class Ingresso {
+	@Id
+	@GeneratedValue
+	private Integer id;
 	
-	private Sessao sessao;
+	@ManyToOne private Sessao sessao;
+	@ManyToOne private Lugar lugar;
+	
 	private BigDecimal preco;
 	
-	public Ingresso(Sessao sessao, Desconto desconto) {
+	@Enumerated(EnumType.STRING)
+	private TipoIngresso tipoIngresso;
+	
+	public Ingresso() {}
+	
+	public Ingresso(Sessao sessao, TipoIngresso tipoIngresso, Lugar lugar) {
 		this.sessao = sessao;
-		this.preco = desconto.calculaDesconto(sessao.getPreco());
+		this.tipoIngresso = tipoIngresso;
+		this.preco = this.tipoIngresso.aplicaDesconto(sessao.getPreco());
+		
 	}
 	
 	public Sessao getSessao() {
@@ -26,5 +44,29 @@ public class Ingresso {
 	}
 	public void setPreco(BigDecimal preco) {
 		this.preco = preco;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public TipoIngresso getTipoIngresso() {
+		return tipoIngresso;
+	}
+
+	public void setTipoIngresso(TipoIngresso tipoIngresso) {
+		this.tipoIngresso = tipoIngresso;
+	}
+
+	public Lugar getLugar() {
+		return lugar;
+	}
+
+	public void setLugar(Lugar lugar) {
+		this.lugar = lugar;
 	}
 }
